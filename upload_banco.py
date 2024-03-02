03b147fa79cb2147af22dc36bb9d51300a10bf56
mayconaranha <74373974+mayconaranha@users.noreply.github.com>
2024-03-02 12:04:01 -0300

    Update upload_banco.py

--- upload_banco.py
+++ upload_banco.py
@@ -12,21 +12,25 @@ start_time = time.time()
 conn = conectar_banco()
 print("Conexão com o banco de dados bem-sucedida.")
 
-
-# SELEÇÃO DE COLUNAS PARA IMPORT
-lista_colunas = ['ID',
-                 'NOME',
-                 'IDADE',
-                 'SEXO',
-                 'DATA'
-                ]
 # DEFINIÇÃO DE TIPO DE DADOS
 tipos_colunas = {'ID': int,
                  'NOME': str,
                  'IDADE': int,
                  'SEXO': str,
-                 'DATA':pd.Timestamp
+                 'DATA': str
                  }
+
+# SELEÇÃO DE COLUNAS PARA IMPORT
+lista_colunas = {
+                'ID',
+                'NOME',
+                'IDADE',
+                'SEXO',
+                'DATA'
+
+}
+
+df = pd.DataFrame (lista_colunas)
 
 # LEITURA DO ARQUIVO EM EXCEL
 df = pd.read_excel('C:\\Users\\mayco\\Desktop\\PROJETOS PYTHON\\BASES\\IMPORT_NOME.xlsx'
@@ -34,7 +38,28 @@ df = pd.read_excel('C:\\Users\\mayco\\Desktop\\PROJETOS PYTHON\\BASES\\IMPORT_NO
                  , usecols = lista_colunas
                 , dtype = tipos_colunas
                 )
-print("Leitura do arquivo bem-sucedida!")
+
+# CONVERSÃO PARA DATA
+df['DATA'] = pd.to_datetime(df['DATA'])
+print(f"Leitura do arquivo bem-sucedida")
+
+# IDENTIFICANDO DATA MIN/MAX
+data_min = df['DATA'].min()
+data_max = df['DATA'].max()
+
+# DELETANDO NO SQL PARA NÃO TER DUPLICIDADE
+query_delete = f"DELETE FROM TBL_D_PESSOAS WHERE DATA >= '{data_min}' AND DATA <= '{data_max}'"
+
+# EXECUTANDO QUERY
+cursor = conn.cursor()
+cursor.execute(query_delete)
+conn.commit()
+
+#insiro dataframe ao banco
+for index, row in df.iterrows():
+    cursor.execute("INSERT INTO TBL_D_PESSOAS (ID,NOME,IDADE,SEXO,DATA) VALUES (?,?,?,?,?)", row.ID, row.NOME, row.IDADE, row.SEXO, row.DATA)
+conn.commit()
+print("Importação ao banco bem-sucedida!")
 
 # ENCERRAR CONEXÃO COM O BANCO
 conn.close()
@@ -46,8 +71,8 @@ end_time = time.time()
 # CALCULO PARA SABER O TEMPO DE PROCESSAMENTO
 elapsed_time = end_time - start_time
 
-df = numero_de_linhas = len(df)
-print(f"Quantidade de linhas importadas: {numero_de_linhas}")
+df = len(df)
+print(f"Quantidade de linhas importadas: {df}")
 print("Tempo para importar: ", convert(elapsed_time))
 
 
